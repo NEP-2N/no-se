3242bce1e793045a8254a4fd18869efabf7d2f31
nep <dogee248@gmail.com>
2026-09-03 08:09:33 -0600

    Update

--- muerete visual/programacion concurrente y paralela/Ordenar.java
+++ muerete visual/programacion concurrente y paralela/Ordenar.java
@@ -1,18 +1,18 @@
 import java.util.Random;
 
-class Burbuja extends Thread{
+class Burbuja extends Thread {
     private int arreglo[];
 
-    public Burbuja(int arreglo[]){
+    public Burbuja(int arreglo[]) {
         this.arreglo = arreglo;
     }
 
     @Override
-    public void run(){
+    public void run() {
 
         boolean ordenado = false;
         int temp;
-        
+
         while (!ordenado) {
             ordenado = true;
 
@@ -29,22 +29,23 @@ class Burbuja extends Thread{
         }
 
     }
-    
+
 }
 
-class Insercion extends Thread{
+
+class Insercion extends Thread {
     private int arreglo[];
 
-    public Insercion(int arreglo[]){
+    public Insercion(int arreglo[]) {
         this.arreglo = arreglo;
     }
 
     @Override
-    public void run(){
+    public void run() {
 
         boolean ordenado = false;
         int temp;
-        
+
         while (!ordenado) {
             ordenado = true;
 
@@ -68,36 +69,14 @@ class Insercion extends Thread{
     }
 }
 
-class QuickSort extends Thread{
+
+class QuickSort extends Thread {
     private int arreglo[];
-
-    public Insercion(int arreglo[]){
-        this.arreglo = arreglo;
-    }
-
-    @Override
-    public void run(){
-
-        boolean ordenado = false;
-        int temp;
-        
-
-    }
-}
-
-public class Ordenar extends Thread {
-    private String algoritmo;
-    private int arreglo[];
-
-    public Ordenar(String algoritmo, int arreglo[]) {
-        this.algoritmo = algoritmo;
-        this.arreglo = arreglo;
-    }
 
     public static void QuickSort(int arreglo[], int bajo, int alto) {
         if (bajo >= alto)
             return;
-        
+
         int pivote = arreglo[alto];
         int a = bajo;
 
@@ -118,74 +97,58 @@ public class Ordenar extends Thread {
         QuickSort(arreglo, a + 1, alto);
     }
 
+    public QuickSort(int arreglo[]) {
+        this.arreglo = arreglo;
+    }
+
     @Override
     public void run() {
 
-        boolean ordenado = false;
-        int temp;
+        QuickSort(arreglo, 0, arreglo.length - 1);
 
-        switch (algoritmo) {
-            case "Burbuja":
-
-            case "Insercion":
-
-            case "QuickSort":
-                QuickSort(arreglo, 0, arreglo.length - 1);
-
-        }
     }
+}
 
+
+public class Ordenar extends Thread {
     public static void main(String[] args) {
-        boolean esperar = false;
-
         int size = 100;
         int arreglo[][] = new int[3][size];
         Random random = new Random();
 
-        for(int i=0; i<3; i++)
+        for (int i = 0; i < 3; i++)
             for (int j = 0; j < size; j++)
                 arreglo[i][j] = random.nextInt(1000);
 
         Burbuja hiloBurbuja = new Burbuja(arreglo[0]);
         Insercion hiloInsercion = new Insercion(arreglo[1]);
-
-        Thread hiloQuickSort = new Thread(new Ordenar("QuickSort", arreglo[2]));
+        QuickSort hiloQuickSort = new QuickSort(arreglo[2]);
 
         hiloBurbuja.start();
-        if(esperar)try{
-            Thread.sleep(100);
-        }catch(InterruptedException e){}
-
         hiloInsercion.start();
-        if(esperar)try{
-            Thread.sleep(100);
-        }catch(InterruptedException e){}
-        
         hiloQuickSort.start();
-        if(esperar)try{
-            Thread.sleep(100);
-        }catch(InterruptedException e){}
 
         try {
             hiloBurbuja.join();
             hiloInsercion.join();
             hiloQuickSort.join();
-        } catch (InterruptedException e) {}
-        
+        } catch (InterruptedException e) {
+        }
+
         System.out.println("Burbuja ha terminado de ordenar los datos: ");
         for (int i = 0; i < arreglo[0].length - 1; i++) {
             System.out.print(arreglo[0][i] + ", ");
         }
         System.out.println(arreglo[0][arreglo[1].length - 1]);
-        
-        
+
+
         System.out.println("Insercion ha terminado de ordenar los datos: ");
         for (int i = 0; i < arreglo[1].length - 1; i++) {
             System.out.print(arreglo[1][i] + ", ");
         }
         System.out.println(arreglo[1][arreglo[1].length - 1]);
-        
-        
+
+
         System.out.println("Quicksort ha terminado de ordenar los datos: ");
         for (int i = 0; i < arreglo[2].length - 1; i++) {
             System.out.print(arreglo[2][i] + ", ");
